2504aba01d7748aef7baf968c14334117e853bea
Steven Sanchez <salvarez14mynledu@gmail.com>
2026-03-20 13:35:16 -0500

    initial backend commit

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,21 @@
+import express from "express"
+import { login, logout, signup,  } from "../controllers/auth.controller.js"
+
+
+
+const router = express.Router()
+
+router.post("/signup", signup)
+
+router.post("/login", login)
+
+router.post("/logout", logout)
+
+// router.put("/update-profile", protectRoute, updateProfile)
+
+
+
+
+
+
+export default router
